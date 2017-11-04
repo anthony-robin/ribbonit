e802fd849284ec68f2745f62552a77da23cc294f
Anthony Robin <contact@anthony-robin.fr>
2017-11-04 21:10:09 +0100

    UPDATE Rubocop sources
    Details:
    - UPGRADE version
    - UPDATE sources
    - FIX offenses

--- lib/ribbonit/configuration.rb
+++ lib/ribbonit/configuration.rb
@@ -8,23 +8,23 @@ module Ribbonit
   def self.configure
     yield configuration
   end
-end
 
-class Ribbonit::Configuration
-  include ActiveSupport::Configurable
+  class Configuration
+    include ActiveSupport::Configurable
 
-  config_accessor(:infos_to_display) do
-    %i[rails_version ruby_version]
+    config_accessor(:infos_to_display) do
+      %i[rails_version ruby_version]
+    end
+    config_accessor(:root_link) { false }
+    config_accessor(:hide_for_small) { true }
+    config_accessor(:position) { 'top-left' }
+    config_accessor(:sticky) { true }
+    config_accessor(:themes) do
+      {
+        development: 'black',
+        staging: 'blue'
+      }
+    end
+    config_accessor(:staging_name) { 'staging' }
   end
-  config_accessor(:root_link) { false }
-  config_accessor(:hide_for_small) { true }
-  config_accessor(:position) { 'top-left' }
-  config_accessor(:sticky) { true }
-  config_accessor(:themes) do
-    {
-      development: 'black',
-      staging: 'blue'
-    }
-  end
-  config_accessor(:staging_name) { 'staging' }
 end
--- lib/ribbonit/rails/engine.rb
+++ lib/ribbonit/rails/engine.rb
@@ -4,7 +4,7 @@ require 'ribbonit/view_helpers'
 module Ribbonit
   module Rails
     class Engine < ::Rails::Engine
-      initializer 'ribbonit.view_helpers' do |app|
+      initializer 'ribbonit.view_helpers' do |_app|
         ActionView::Base.send :include, ViewHelpers
         ActiveSupport.on_load(:action_controller) do
           append_view_path "#{Gem.loaded_specs['ribbonit'].full_gem_path}/lib/app/views/partials"
